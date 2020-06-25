7e2ce4eb83ece23654290fd3e38636b6ea173092
DavidZ <davidbill@outlook.com>
2020-06-25 10:37:31 +0800

    Site updated: 2020-6-25 10:37:30

---
+++ scripts/deploy.ps1
@@ -0,0 +1,23 @@
+$CurrentWorKDir = Get-Location
+$CurrentDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$Message = "Site updated: $(Get-Date -Format 'yyyy-M-d H:m:s')"
+Write-Host $Message
+
+
+Write-Host "===================="
+Write-Host "Git push the resources"
+Set-Location $CurrentDir
+Set-Location ..
+git add .
+git commit -am $Message
+
+Write-Host "===================="
+Write-Host "Generate static site"
+# hugo.exe -D
+
+Write-Host "===================="
+Write-Host "Git push the generated static site"
+# Set-Location public
+# Get-Location
+
+Set-Location $CurrentWorKDir
